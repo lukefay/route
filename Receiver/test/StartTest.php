1b86b3abd0c418f6d21a31bd5ae17294f2cbe51d
lukefay <lfy460@hotmail.com>
2026-02-25 15:50:23 -0800

    Delete Receiver directory
    Separating out ROUTE protocol vs. Application layer rendering.

--- Receiver/test/StartTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-	chdir('/var/www/Work/Route_Sender/bin/');
-	exec("sudo nice --20 ./Start2.sh 1000 > ServiceLog.txt &");
-	exit;
-?>
--- Receiver/test/Testteardown.php
+++
@@ -1,6 +0,0 @@
-<?php
-	exec("sudo killall Start2.sh");
-	exec("sudo killall flute_sender");
-	exec("sudo killall flute");
-	exit;
-?>
